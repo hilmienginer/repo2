d8debac1e349ce6731ccd9e8bdb6156253e98824
Turan <84424530+turannul@users.noreply.github.com>
2022-01-30 03:23:34 +0300

    Devir-Teslim işlemi

--- sign.sh
+++
@@ -1,5 +0,0 @@
-#! /opt/procursus/bin/bash
-# sign.sh # Commands creates public and secret key #
-gpg -abs -u 0EE28B71278A94D0EF60F694B28DCC616F750DDF -o Release.gpg Release #<-- Public Key
-#gpg --export #0EE28B71278A94D0EF60F694B28DCC616F750DDF > #turannul-repo.gpg #<-- Secret Key
-#For some reason on unc0ver jailbreak you can't create #the keys
